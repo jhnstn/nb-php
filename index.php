aa456728ae1bfc8bb1e72633c0c669c4ab2b2b17
Jason Johnston <johnston@nationbuilder.com>
2013-10-03 20:57:28 -0700

    show my people

--- index.php
+++ index.php
@@ -3,9 +3,37 @@ require('lib/oauth2/Client.php');
 require('lib/oauth2/GrantType/IGrantType.php');
 require('lib/oauth2/GrantType/AuthorizationCode.php');
 
-$clientId = 'yourClientId';
-$clientSecret = 'yourClientSecret';
-$client = new OAuth2\Client($clientId, $clientSecret);
+$clientId     = 'b8988631425a9c2856f69df878265576d602ee512472eac89c3e75614a24e9d9';
+$clientSecret = '58ea2ff4bb1a5a3a4b33be5cb7a3e687c194f40bf5b3196a60d1c5c387956af6';
 
-phpinfo();
+$redirectUri    = 'http://nb-php.herokuapp.com/';
+$authorizeUri   = 'https://jasonjohnston.nationbuilder.com/oauth/authorize';
+$accessTokenUri = 'https://jasonjohnston.nationbuilder.com/oauth/token';
+
+$client       = new OAuth2\Client($clientId, $clientSecret);
+
+if(!isset($_GET['code'])) {
+  $authUrl = $client->getAuthenticationUrl($authorizeUri, $redirectUri);
+  header('Location: ' . $authUrl);
+  die('Redirect');
+}
+else {
+  $params = array('code' => $_GET['code'], 'redirect_uri' => $redirectUri );
+  $response = $client->getAccessToken($accessTokenUri,'authorization_code',$params);
+  $client->setAccessToken($response['result']['access_token']);
+  $people = $client->fetch('https://jasonjohnston.nationbuilder.com//api/v1/people');
+}
 ?>
+
+
+<h1> People </h1>
+
+<?php foreach( $people as $person ): ?>
+
+  <div>
+    <span> <?php echo $person['first_name']; ?> </span>
+    &nbsp;
+    <span> <?php echo $person['last_name'];  ?> </span>
+  </div>
+
+<?php endforeach; ?>
